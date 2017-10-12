0764d73db81526c37a6a5f6d62ff40d62008d266
disjain <dishang.jain@capgemini.com>
2017-10-12 01:55:48 -0400

    Projectv1.1.0 Modified and set everything to make project running

---
+++ HotelBookingManagementSystem/src/com/capgemini/hotelbooking/services/ICommonService.java
@@ -0,0 +1,16 @@
+package com.capgemini.hotelbooking.services;
+
+import java.util.List;
+
+import com.capgemini.hotelbooking.beans.HotelBean;
+import com.capgemini.hotelbooking.beans.RoomBean;
+import com.capgemini.hotelbooking.beans.UserBean;
+import com.capgemini.hotelbooking.exceptions.BookingException;
+
+public interface ICommonService {
+	public void registerUser(UserBean userBean) throws BookingException;
+	public UserBean login(String username, String password) throws BookingException;
+	List<HotelBean> retrieveHotels() throws BookingException;
+	List<RoomBean> retrieveRooms() throws BookingException;
+	List<String> retrieveUserNames() throws BookingException;
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 This is java web based Hotel Booking Management System.
 Technologies used are Spring IOC, JPA, AnglurJS and jQuery.
+
+1.1.0
