7da1509199c27f082608a555476a4ab1c31a60ea
dishangjain <jaindishang24@gmail.com>
2017-10-19 12:37:20 +0530

    Update dateValidation.js

--- HotelBookingManagementSystem/WebContent/assets/js/dateValidation.js
+++ HotelBookingManagementSystem/WebContent/assets/js/dateValidation.js
@@ -1,3 +1,9 @@
-/**
- * 
- */
+function setMin(){
+checkin.min = new Date().toISOString().slice(0,10);
+}
+function validate(){
+var cInDate  = new Date(checkin.value);
+var cOutDate = new Date(cInDate);
+cOutDate.setDate(cInDate.getDate()+1);
+checkout.min = cOutDate.toISOString().slice(0,10);
+}
